ed176b15034c38cebe1ed6cfd5ef89f84d8144e4
Tiago Borges <tiagoborges2940@gmail.com>
2022-04-27 09:21:07 -0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,51 @@
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = true;
+        boolean b3 = true;
+        boolean b4 = true;
+
+        System.out.println("b1 && b2 " + (b1 && b2));
+        System.out.println("b1 && b3 " + (b1 && b3));
+
+        System.out.println("b2 || b3 " + (b2 || b3));
+        System.out.println("b2 || b4 " + (b2 || b4));
+
+        System.out.println("b1 ^ b3 " + (b1 ^ b3));
+        System.out.println("b4 ^ b1 " + (b4 ^b1));
+
+        System.out.println(!b1);
+        System.out.println(!b2);
+
+        int i1 = 10;
+        int i2 = 5;
+
+        float f1 = 20f;
+        float f2 = 50f;
+
+        System.out.println("((i1 + i2) < (f2 - f1)) && true " + (((i1 + i2) < (f2 - f1)) && true));
+        System.out.println("((i1 > i2) || (f2 < f1)) " + ((i1 > i2) || (f2 < f1)));
+
+        double salarioMensal = 11953.58d;
+        double mediaSalario = 10500d;
+
+        int quantidadedeDependentes = 4;
+        int mediaDependentes = 2;
+
+        System.out.println((salarioMensal < mediaSalario) && (quantidadedeDependentes >= mediaDependentes));
+
+        boolean salarioBaixo = salarioMensal < mediaSalario;
+        boolean muitosDependentes = quantidadedeDependentes >= mediaDependentes;
+
+        System.out.println((salarioBaixo) && (muitosDependentes));
+
+        boolean recebeAuxilo = (salarioBaixo) && (muitosDependentes);
+        System.out.println("recebeAuxilio " + (recebeAuxilo));
+
+
+    }
+}
